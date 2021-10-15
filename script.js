6e539420b4a60c1095aa9b9f8feda3183ca5b1c4
Yabluko <83119163+VladVertehel@users.noreply.github.com>
2021-10-15 15:43:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.button').onclick = function(){
+    alert('XAXAXAXAXAXAXAXAXAXAXA');
+    this.classList.add('active');
+    this.innerText = 'NOW YOUR SOUL IS MINE (◣‿◢)و ̑̑';
+    document.querySelector('.btn').classList.add('active_btn');
+    document.querySelector('.img').src='hell-hound.jpg';
+    document.querySelector('body').classList.add('pressed');
+}
